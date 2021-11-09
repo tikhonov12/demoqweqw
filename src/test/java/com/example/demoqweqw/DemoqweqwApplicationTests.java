7704ead3fd0cd7f2fd41da1444cb367ec9473e27
tihonov10101 <tihonov10101@gmail.com>
2021-11-09 12:13:26 +0500

    Initial commit

---
+++ src/test/java/com/example/demoqweqw/DemoqweqwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoqweqw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoqweqwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
